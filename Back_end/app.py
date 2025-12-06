8f07f541c91906f291e9c822adcbbc8245b1746e
imen <yousriimen09@gmail.com>
2025-12-06 09:32:00 +0100

    Change URL

--- Back_end/app.py
+++ Back_end/app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "https://tp-algo-three.vercel.app/"}})
+CORS(app, resources={r"/*": {"origins": "https://tp-algo-three.vercel.app"}})
 
 # @app.route("/")
 
